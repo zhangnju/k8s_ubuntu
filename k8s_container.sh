8708986a3c01ec2b1d27794b5c309942417cb269
zhangnju <zhangnju@126.com>
2020-08-08 09:03:26 +0800

    update

--- k8s_container.sh
+++ k8s_container.sh
@@ -3,13 +3,13 @@
 echo "Downloading K8S docker"
 
 images=(  # 下面的镜像应该去除"k8s.gcr.io/"的前缀，版本换成上面获取到的版本
-     kube-apiserver:v1.18.6
-     kube-controller-manager:v1.18.6
-     kube-scheduler:v1.18.6
-     kube-proxy:v1.18.6
-     pause:3.2
+     kube-apiserver:v1.17.9
+     kube-controller-manager:v1.17.9
+     kube-scheduler:v1.17.9
+     kube-proxy:v1.17.9
+     pause:3.1
      etcd:3.4.3-0
-     coredns:1.6.7
+     coredns:1.6.5
 )
 
 for imageName in ${images[@]} ; do
